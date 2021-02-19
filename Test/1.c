4e63fcc91bcf39beab5f5f4bc5c5ff9040234661
Hello-Chen <2421985222@qq.com>
2021-02-19 13:52:19 +0800

    first commit

---
+++ Test/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char str[]="sdad";
+    char str1[100];
+    printf("%ld\n",sizeof(str));
+    printf("%lu\n",strlen(str));
+    printf("%s\n",str);
+
+    strcpy(str1,str);
+    printf("%s %s\n",str,str1);
+    return 0;
+}
