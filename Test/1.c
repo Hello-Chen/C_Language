bd7868dc821f13853d93809bdf4aacef2ddc00cf
Hello-Chen <2421985222@qq.com>
2021-03-09 21:02:17 +0800

    1

--- Test/1.c
+++ Test/1.c
@@ -2,7 +2,14 @@
 
 int main(void)
 {
-    
-    
+    char a[] = "programming", b[] = "language";
+    char *p1, *p2;
+    p1 = a, p2 = b;
+    for (int i = 0; i < 7; i++)
+    {
+        if (*(p1 + i) == *(p2 + i))
+            printf("%c", *(p1 + i));
+    }
+
     return 0;
 }
